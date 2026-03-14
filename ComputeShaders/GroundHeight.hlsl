edda09dc965fcea1cbf133c803f2e77c565de6b5
Jonas Wischeropp <jonas@wischeropp.de>
2026-03-14 14:29:19 +0100

    remove treated todo comment

--- ComputeShaders/GroundHeight.hlsl
+++ ComputeShaders/GroundHeight.hlsl
@@ -6,7 +6,6 @@
 Texture2D<float> GroundHeight;
 
 float GetGroundHeight(uint x, uint y, float3 size) {
-    // TODO is an adjustment according to NDC required?
 #if UNITY_REVERSED_Z
     return GroundHeight[uint2(x,y)] *  size.y;
 #else
